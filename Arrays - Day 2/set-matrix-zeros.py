5e267be4324bc93cbcabc9d468c1a37e2f2e6070
= <danishfaisal.wwe@gmail.com>
2021-06-21 22:44:46 +0530

    set-matrix-zeroes comments

--- Arrays - Day 2/set-matrix-zeros.py
+++ Arrays - Day 2/set-matrix-zeros.py
@@ -7,6 +7,7 @@ class Solution:
         """
         rows = []
         cols = []
+        # get the indexes row and column wise where 0 is present in rows & cols list
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 if matrix[i][j]==0:
@@ -16,8 +17,10 @@ class Solution:
             return
         m=len(matrix)
         n=len(matrix[0])
+        # make total row to 0 for an index in rows list
         for r in rows:
             matrix[r] = [0]*n
+        # make a col to 0 for index in cols list
         for c in cols:
             for i in range(m):
                 matrix[i][c]=0
