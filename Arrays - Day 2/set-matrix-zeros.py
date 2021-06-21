7e6e2f4fc4b3c0e51f53d905001ed6150474fec5
= <danishfaisal.wwe@gmail.com>
2021-06-21 22:40:59 +0530

    set-matrix-zeroes code

--- Arrays - Day 2/set-matrix-zeros.py
+++ Arrays - Day 2/set-matrix-zeros.py
@@ -1,1 +1,23 @@
 # https://leetcode.com/problems/set-matrix-zeroes/
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        rows = []
+        cols = []
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j]==0:
+                    rows.append(i)
+                    cols.append(j)
+        if len(rows)==0 and len(cols)==0:
+            return
+        m=len(matrix)
+        n=len(matrix[0])
+        for r in rows:
+            matrix[r] = [0]*n
+        for c in cols:
+            for i in range(m):
+                matrix[i][c]=0
