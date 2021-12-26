2498ea4356aa1a3e02050511011e23a340f47797
= <danishfaisal.wwe@gmail.com>
2021-12-26 21:37:28 +0530

    Implement Queue using Stacks

--- Stack & Queue - Day 13/implement-queue-using-stacks.py
+++ Stack & Queue - Day 13/implement-queue-using-stacks.py
@@ -1,3 +1,4 @@
+# https://leetcode.com/problems/implement-queue-using-stacks/
 # https://www.geeksforgeeks.org/queue-using-stacks/
 
 # Approach 2: TC: AMORTIZED O(1) for pop-op, SC:O(2N)
@@ -83,3 +84,34 @@ if __name__ == '__main__':
     print(q.Dequeue())
     print(q.Dequeue())
     print(q.Dequeue())
+
+# On leetcode
+class MyQueue:
+
+    def __init__(self):
+        self.s1=[]
+        self.s2=[]
+
+    def push(self, x: int) -> None:
+        while len(self.s1)>0:
+            self.s2.append(self.s1.pop())
+        self.s1.append(x)
+        while len(self.s2)>0:
+            self.s1.append(self.s2.pop())
+            
+    def pop(self) -> int:
+        return self.s1.pop()
+
+    def peek(self) -> int:
+        return self.s1[-1]
+
+    def empty(self) -> bool:
+        return len(self.s1)==0
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
