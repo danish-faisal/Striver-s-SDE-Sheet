c40c2ed9580aa765017efb95943dadaee8bd11e7
= <danishfaisal.wwe@gmail.com>
2022-03-11 22:42:04 +0530

    Detect Cycle in Undirected Graph using DFS

--- Graphs - Day 23/Detect-Cycle-in-Undirected-Graph-using-DFS.py
+++ Graphs - Day 23/Detect-Cycle-in-Undirected-Graph-using-DFS.py
@@ -7,21 +7,30 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph = [[] for _ in range(numCourses)]
-        visit = [0 for _ in range(numCourses)]
-        for x, y in prerequisites:
+        visited = [0 for _ in range(numCourses)]
+        # create graph
+        for pair in prerequisites:
+            x, y = pair
             graph[x].append(y)
-        def dfs(i):
-            if visit[i] == -1:
-                return False
-            if visit[i] == 1:
-                return True
-            visit[i] = -1
-            for j in graph[i]:
-                if not dfs(j):
-                    return False
-            visit[i] = 1
-            return True
+        # visit each node
         for i in range(numCourses):
-            if not dfs(i):
+            if not self.dfs(graph, visited, i):
                 return False
+        return True
+    
+    def dfs(self, graph, visited, i):
+        # if ith node is marked as being visited, then a cycle is found
+        if visited[i] == -1:
+            return False
+        # if it is done visted, then do not visit again
+        if visited[i] == 1:
+            return True
+        # mark as being visited
+        visited[i] = -1
+        # visit all the neighbours
+        for j in graph[i]:
+            if not self.dfs(graph, visited, j):
+                return False
+        # after visit all the neighbours, mark it as done visited
+        visited[i] = 1
         return True
